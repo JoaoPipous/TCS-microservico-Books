41ffee316c64f022c2b34a002c30ba30a8d472af
JoaoPipous <JoaoDr_340@outlook.com>
2025-07-03 12:39:58 -0300

    Initial commit

---
+++ src/main/java/com/TCS/microservico_Books/MicroservicoBooksApplication.java
@@ -0,0 +1,13 @@
+package com.TCS.microservico_Books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicoBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicoBooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TCS/microservico_Books/MicroservicoBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TCS.microservico_Books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicoBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
